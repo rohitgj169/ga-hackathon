3e9549dac44de7b353496a2c79fa618c02d7c3f7
Kareem Draz <kareemdraz@myyz-0792.investcloud.int>
2021-08-16 14:18:15 -0400

    Folder testing

---
+++ src/components/API/API.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class API extends Component {
+    render() {
+        return (
+            <div>
+                <h1>API</h1>
+            </div>
+        )
+    }
+}
+
+export default API
