fe873e0896ef121fbb839a38d29d5bbb14fe4a50
RohitGJ <77436246+rohitgj169@users.noreply.github.com>
2021-08-17 12:13:04 -0400

    tweaks

--- src/App.js
+++ src/App.js
@@ -6,18 +6,20 @@ import { Mongoose } from "mongoose";
 import ProjectsIndex from "./pages/ProjectsIndex/ProjectsIndex";
 
 function App() {
-  const responseGoogle = (response) => {
+
+  const handleLogin = (response) => {
     console.log(response);
     console.log(response.profileObj);
   };
+
   return (
     <div className="App">
       App
       <GoogleLogin
-        clientId="893359449772-dd1ri95gq198m28a1k6t534k35fr0ovk.apps.googleusercontent.com"
+        clientId={process.env.CLIENT_ID}
         buttonText="Login"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
+        onSuccess={handleLogin}
+        onFailure={handleLogin}
         cookiePolicy={"single_host_origin"}
       />
       <Switch>
