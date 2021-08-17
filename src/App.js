d15ff494aacbf14619fba5f3705c94a4953b638a
Kareem Draz <kareemdraz@myyz-0792.investcloud.int>
2021-08-16 23:40:14 -0400

    OAUTHHHH

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import GoogleLogin from 'react-google-login'
+import { Mongoose } from 'mongoose';
 
 function App() {
+
+
+  const responseGoogle = (response) => {
+    console.log(response)
+    console.log(response.profileObj)
+  }
   return (
     <div className="App">
       App
+      <GoogleLogin 
+        clientId="893359449772-dd1ri95gq198m28a1k6t534k35fr0ovk.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+      />
     </div>
   );
 }
