4b1646dbff68e52e5aacd13b94bc1dc666547d90
RohitGJ <77436246+rohitgj169@users.noreply.github.com>
2021-08-19 11:44:43 -0400

    populate

--- controllers/api/projects.js
+++ controllers/api/projects.js
@@ -2,7 +2,7 @@ const Project = require("../../models/project");
 
 async function index(req, res) {
   try {
-    const projects = await Project.find({});
+    const projects = await Project.find({}).populate('creator');
     res.status(200).json(projects);
   } catch (err) {
     res.status(400).json("failed to retrieve projects");
